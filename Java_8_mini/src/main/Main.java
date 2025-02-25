6b793f50609fdefd2e8ddc122af06223ddeadfdf
Ricardo Cuthbert <ricardocuthbertjr@gmail.com>
2025-02-25 20:25:12 +0700

    Session 8

--- Java_8_mini/src/main/Main.java
+++ Java_8_mini/src/main/Main.java
@@ -16,12 +16,13 @@ public class Main {
 	File file = new File(path);
 
 	public Main() {
-		
+
 		int choice = 0;
 		try {
-			if(file.createNewFile()) {
+			if (file.createNewFile()) {
 				System.out.println("Successfully created " + file.getName());
-			}else {
+			} else {
+				fillList();
 				System.out.println("File " + file.getName() + " already exist!");
 			}
 		} catch (IOException e) {
@@ -36,32 +37,36 @@ public class Main {
 			System.out.println("4. Delete Song");
 			System.out.println("5. Exit");
 			System.out.print(">> ");
-			
+
 			try {
 				choice = scan.nextInt();
 			} catch (Exception e) {
 				System.err.println("Please input integer!");
 			}
 			scan.nextLine();
-			
+
 			switch (choice) {
 			case 1: {
 				addSong();
 				pressEnter();
 				break;
-			}case 2:{
+			}
+			case 2: {
 				viewSong();
 				pressEnter();
 				break;
-			}case 3: {
+			}
+			case 3: {
 				searchSong();
 				pressEnter();
 				break;
-			}case 4:{
+			}
+			case 4: {
 				deleteSong();
 				pressEnter();
 				break;
-			}case 5:{
+			}
+			case 5: {
 				System.out.println("Bye bye T^T");
 				file.delete();
 				System.exit(0);
@@ -70,12 +75,12 @@ public class Main {
 			}
 		} while (choice != 5);
 	}
-	
+
 	public void addSong() {
 		System.out.print("Input Song title: ");
 		String title = scan.nextLine();
 		song.add(new Song(title));
-		
+
 		try {
 			FileWriter fw = new FileWriter(file, true);
 			fw.write(title + "\n");
@@ -85,23 +90,24 @@ public class Main {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void viewSong() {
 		try {
 			BufferedReader bf = new BufferedReader(new FileReader(file));
 			String firstLine = bf.readLine();
-			if(firstLine == null) {
+			if (firstLine == null) {
 				System.out.println("File is empty");
 				bf.close();
 				return;
 			}
-			
+
 			System.out.println("Song List: ");
 			System.out.println("- " + firstLine);
-			
+
 			String line;
 			while ((line = bf.readLine()) != null) {
-				System.out.println("- " + line);;
+				System.out.println("- " + line);
+				;
 			}
 			bf.close();
 		} catch (IOException e) {
@@ -109,14 +115,14 @@ public class Main {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void searchSong() {
 		viewSong();
 		System.out.print("Song title: ");
 		String title = scan.nextLine();
 		Boolean cek = true;
 		String lines;
-		
+
 		BufferedReader bf;
 		try {
 			bf = new BufferedReader(new FileReader(file));
@@ -131,25 +137,26 @@ public class Main {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		if (cek) System.out.println("Song is not found");
+
+		if (cek)
+			System.out.println("Song is not found");
 	}
-	
+
 	public void deleteSong() {
 		viewSong();
 		System.out.print("Song's title that want to delete: ");
 		String title = scan.nextLine();
 		Boolean cek = true;
-		
+
 		for (int i = 0; i < song.size(); i++) {
-			if(song.get(i).getTitle().equalsIgnoreCase(title)) {
+			if (song.get(i).getTitle().equalsIgnoreCase(title)) {
 				String temp = song.get(i).getTitle();
 				song.remove(i);
 				cek = false;
 				System.out.println(temp + " deleted successfully!");
 			}
 		}
-		
+
 		try {
 			FileWriter fw = new FileWriter(file);
 			for (Song lagu : song) {
@@ -160,10 +167,26 @@ public class Main {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		if (cek) System.out.println("Song not found!");
+
+		if (cek)
+			System.out.println("Song not found!");
 	}
 	
+	public void fillList() {
+		String lines;
+		BufferedReader bf;
+		try {
+			bf = new BufferedReader(new FileReader(file));
+			while ((lines = bf.readLine()) != null) {
+				song.add(new Song(lines));
+			}
+			bf.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 	public void pressEnter() {
 		System.out.println("Press enter to continue...");
 		scan.nextLine();
